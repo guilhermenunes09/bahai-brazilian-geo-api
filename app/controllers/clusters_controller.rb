e59a0e852f2284dfa5065cddf4ff70eab0eae4d6
Guilherme Nunes <guilhermenunes09@protonmail.com>
2024-01-02 11:20:59 -0300

    options to update clusters

--- app/controllers/clusters_controller.rb
+++ app/controllers/clusters_controller.rb
@@ -1,6 +1,23 @@
 class ClustersController < ApplicationController
+  before_action :set_cluster, only: [:update]
+  
   def index
     @clusters = Cluster.all
     render json: @clusters, each_serializer: ClusterSerializer
+  end
+
+  def update
+    @cluster.update(cluster_params)
+    render json: @cluster, each_serializer: ClusterSerializer
+  end
+
+  private
+
+  def cluster_params
+    params.require(:cluster).permit(:id, :name, :milestone)
+  end
+
+  def set_cluster
+    @cluster = Cluster.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :cities, only: [:index]
-  resources :clusters, only: [:index]
+  resources :clusters, only: [:index, :update]
   resources :zones, only: [:index]
   resources :states, only: [:index]
   resources :regions, only: [:index]
