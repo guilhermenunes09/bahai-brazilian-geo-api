b90b644ea9584850100de59f613145f79535b564
Guilherme Nunes <guilhermenunes09@protonmail.com>
2026-03-31 03:59:45 -0300

    Remove unused seed file loads from seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,10 +3,6 @@
 
 
 # Load seed files in this order
-load File.expand_path('seeds/seeds_countries.rb', __dir__)
-load File.expand_path('seeds/seeds_regions.rb', __dir__)
-load File.expand_path('seeds/seeds_bahai_zones.rb', __dir__)
+
 load File.expand_path('seeds/seeds_bahai_clusters.rb', __dir__)
-load File.expand_path('seeds/seeds_bahai_regions.rb', __dir__)
-load File.expand_path('seeds/seeds_states.rb', __dir__)
-load File.expand_path('seeds/seeds_cities.rb', __dir__)
+
